491fb15331e140973be7aea826750c4aa31682f1
Sergio Mercado Nuñez <sm9276@g.rit.edu>
2024-08-27 13:03:46 -0400

    slight adjustments

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup {
-    ensure_installed = { 'vim', 'vimdoc', 'lua', 'cpp','c','rust','python','json','csv' },	
+    ensure_installed = { 'vim', 'vimdoc', 'lua', 'cpp','c','rust','python','json','csv','make'},	
 	sync_install = false,
     auto_install = false,
     highlight = { enable = true },
--- lua/sm9276/keybinds.lua
+++ lua/sm9276/keybinds.lua
@@ -10,13 +10,6 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set("n", "<leader>vwm", function()
-    require("vim-with-me").StartVimWithMe()
-end)
-vim.keymap.set("n", "<leader>svwm", function()
-    require("vim-with-me").StopVimWithMe()
-end)
-
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
@@ -28,7 +21,6 @@ vim.keymap.set("n", "<leader>Y", "\"+Y")
 vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
 
--- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
